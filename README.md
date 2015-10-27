409f0c01a0ff9d3099b5ee0d1caecd70daa4ce3b
Paul Martin <Paul.martin@digital.cabinet-office.gov.uk>
2015-10-27 17:34:00 +0000

    test create

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # vmtap
+
+vmtap is a personal Ruby project using Digital Ocean's dropletkit:
+https://github.com/digitalocean/droplet_kit
+
+My aim is to provide a clean interface using a set of environment
+variables to spin up VMs with all my requirements.
--- lib/main.rb
+++ lib/main.rb
@@ -1,4 +1,18 @@
+require 'yaml'
+require 'trollop'
 require 'droplet_kit'
+require 'randexp'
+
+opts = Trollop::options do
+  opt :name, "Name of VM", :type => :string
+end
+
+unless opts[:name]
+  puts "Generating random hostname"
+  name = /\w{8}/.gen
+else
+  name = opts[:name]
+end
 
 token = ENV['OCEAN_TOKEN']
 unless token
@@ -6,3 +20,17 @@ unless token
 end
 
 client = DropletKit::Client.new(access_token: token)
+
+vmspec = YAML.load_file('config/vmspec.yaml')
+vms = YAML.load_file('config/vms.yaml')
+
+default = vmspec["default"]
+region = default[:region]
+image  = default[:image]
+size   = default[:size]
+
+droplet = DropletKit::Droplet.new(name: name, region: region, image: image, size: size)
+puts "Creating machine called " + name
+client.droplets.create(droplet)
+
+puts "Done!"
